b5a41d20e78bb00df3f044617eb272da0ac321e5
kozlowskipawelwork <kozlowskipawelwork@gmail.com>
2023-03-17 02:16:00 +0100

    moved  Products interface to types.ts (obviously)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Product } from './types';
 
-interface Product {
-  id: number;
-  title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
-}
+
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
