efc43579808387999e9b32ca4fe3cc309f7aa58c
kozlowskipawelwork <kozlowskipawelwork@gmail.com>
2023-03-17 01:55:40 +0100

    got that basic table running

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://dummyjson.com/products');
+      setProducts(result.data.products);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleDelete = (productId: number) => {
+    setProducts(products.filter(product => product.id !== productId));
+  };
+
   return (
-    <div >
-     
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Stock</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product.id}>
+              <td>{product.title}</td>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+              <td>{product.rating}</td>
+              <td>{product.stock}</td>
+              <td>{product.brand}</td>
+              <td>{product.category}</td>
+              <td>
+                <button onClick={() => handleDelete(product.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
