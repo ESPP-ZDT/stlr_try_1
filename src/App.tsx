bdbeb368239bf61e2f361864049d34fb912891f9
kozlowskipawelwork <kozlowskipawelwork@gmail.com>
2023-03-17 03:30:59 +0100

    moved the table view to other ProductTable.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Product } from './types';
-
-
+import ProductTable from './components/ProductTable';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -22,36 +21,7 @@ function App() {
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Rating</th>
-            <th>Stock</th>
-            <th>Brand</th>
-            <th>Category</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => (
-            <tr key={product.id}>
-              <td>{product.title}</td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>
-              <td>{product.rating}</td>
-              <td>{product.stock}</td>
-              <td>{product.brand}</td>
-              <td>{product.category}</td>
-              <td>
-                <button onClick={() => handleDelete(product.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ProductTable products={products} onDelete={handleDelete} />
     </div>
   );
 }
---
+++ src/components/ProductTable.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Product } from '../types';
+
+interface Props {
+  products: Product[];
+  onDelete: (productId: number) => void;
+}
+
+const ProductTable: React.FC<Props> = ({ products, onDelete }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Stock</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product.id}>
+              <td>{product.title}</td>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+              <td>{product.rating}</td>
+              <td>{product.stock}</td>
+              <td>{product.brand}</td>
+              <td>{product.category}</td>
+              <td>
+                <button onClick={() => onDelete(product.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
